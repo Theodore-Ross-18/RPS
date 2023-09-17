765b0716c64b55cbf80851729ab2f7d7a31c424c
Theodore Ross <115604507+Theodore-Ross-18@users.noreply.github.com>
2023-09-17 22:16:19 +0800

    Revise
    Added the revise version of RPS

---
+++ RPS.2.py
@@ -0,0 +1,96 @@
+
+# The revised code version of the RPS 1
+# A program that will welcome the user by entering their name
+# A rock, paper, and scissor game with three (3) rounds 
+# You versus the computer (Who will win?)
+
+import random
+
+print("----------------------------")
+print("> Press |Enter| to Continue.")
+print("> Input |Q| to quit the Game")
+print("> Input |T| to know the developer")
+print("---------------------------------")
+game = input("You v/s Computer  -- Who will win? > ").upper()
+print("-------------------------")
+
+if game == "":
+    player = input("What's your name? > ").upper()
+    played = player
+
+    print("-------------------------")
+    print("-----Welcome to RPS!-----")
+    print("------------------------------------")
+    print(f"> Hello {player} are you the chosen one? ")
+    print("------------------------------------")
+
+    RPS = ["Rock", "r", "Paper", "p", "Scissor", "s"]
+
+    print("---" + RPS[0] + "--" + RPS[2] + "--" + RPS[4] + "--")
+    print("--------------------------------")
+
+    # Variables set to both 0
+    player_score = 0
+    computer_score = 0
+    game_round = 1
+    
+    # While loop
+    while player_score or computer_score < 3:
+        player_choices = ["r", "p", "s"]
+        print(f"{RPS[0]} = r  {RPS[2]} = p  {RPS[4]} = s")
+        print("--------------------------------")
+        computer = random.choice(player_choices)
+    
+        player = str(input("Enter your Move > ")).lower()
+        print("-------------------------")
+        if player == computer:
+            print(f"(Round {game_round})")
+            print("> Whoa....it's a tie")
+            print("--------------------------------")
+            game_round += 1
+    
+        elif player == "r" and computer == "p" or player == "s" and computer == "r" or player == "p" and computer == "s":
+            computer_score = computer_score + 1
+            print(f"> Computer Won... (Round {game_round})")
+            print("-------------------------")
+            print(f"Computer score is = {computer_score}")
+            print("Response: Try Again Human")
+            print("--------------------------------")
+            if computer_score == 3:
+                print("I win the game, you cannot defeat me")
+                print("------------------------------------")
+
+                break
+            game_round += 1
+        else:
+            if player == "p" and computer == "r" or player == "r" and computer == "s" or player == "s" and computer == "p":  
+                player_score = player_score + 1
+                print(f"> You won... (Round {game_round})")
+                print("-------------------------")
+                print(f"Your score is = {player_score}")
+                print("--------------------------------")
+                if player_score == 3:
+                    print("You win...")
+                    print("-------------------------")
+                
+                    break
+            game_round +=1  
+
+elif game == "Q":
+    print("Farewell Warrior...")
+    print("-------------------------")
+
+elif game == "T":
+    # Developer information
+    developer = ["Theodore Ross C. Bermejo", "1st Year", "BSIT", "Block 2", "Palawan State University"
+    , "College of Sciences", "RPS", "February", 14, 2023, "Single"]
+    print(f"Name: {developer[0]}")
+    print(f"Status: {developer[-1]}")
+    print(f"School: {developer[-7]}")
+    print(f"Department: {developer[-6]}")
+    print(f"Year/Program/Block: {developer[1]} - {developer[2]} - {developer[3]}")
+    print(f"Game name: {developer[-5]}")
+    print(f"Date Started: {developer[-4]} {developer[-3]}, {developer[-2]}")
+
+else: 
+    print("Invalid Input!")
